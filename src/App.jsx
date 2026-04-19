faebaea3839db1618cd3e584406157fa2953697a
esam <emmanueldaud358@gmail.com>
2026-04-19 16:09:31 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Phase1 from './components/phase1'; 
 import Login from './pages/Login';         
 import Vacancies from './pages/Vacancies'; 
-
+import Signup from './pages/Signup';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Phase1 />} />
       <Route path="/login" element={<Login />} />
       <Route path="/vacancies" element={<Vacancies />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
