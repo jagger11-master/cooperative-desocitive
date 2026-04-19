a33ee0b7042e8c646453d23ad83afaa2557650cd
esam <emmanueldaud358@gmail.com>
2026-04-19 03:17:25 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Phase1 from './components/phase1';
+
+function App() {
+  return (
+    <div>
+      <Phase1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/phase1.jsx
@@ -0,0 +1,29 @@
+import logo from "../assets/charwe.png";
+
+
+
+function Phase1() {
+    return (
+
+        <section className="h-screen flex flex-col items-center justify-center bg-[#0a0a0b] px-6 text-center">
+            <img src={logo} alt="Charwe Logo" className="h-48 w-auto relative -top-12 mb-6 rounded-3xl" />
+
+
+            <h3 className="text-5xl font-black text-slate-200 mb-10">
+               Welcome to Charwe Media
+                
+            </h3>
+            <p className="text-xl text-slate-300 max-w-2xl mb-10">
+                Charwe-media is an online platform designed to enable Tanzanians to dedicated storrytellings,
+                branding solutions and media production.
+            </p>
+
+            <button className="bg-orange-950 text-white px-8 py-3 rounded-full font-bold">
+                Browse Vacancies
+            </button>
+        </section>
+
+    );
+}
+export default Phase1;
+
