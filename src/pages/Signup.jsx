d638d818d167ce007af1b72131a64d7265efa768
esam <emmanueldaud358@gmail.com>
2026-04-21 00:05:21 +0300

    commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -15,10 +15,28 @@ export default function Signup() {
     e.preventDefault();
     let newErrors = {};
 
-    if (!formData.username) newErrors.username = "This field is required";
-    if (!formData.email) newErrors.email = "This field is required";
-    if (!formData.password) newErrors.password = "This field is required";
-    if (formData.password !== formData.confirm) newErrors.confirm = "Passwords do not match";
+    const usernameRegex = /^[a-zA-Z\s]+$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!formData.username) {
+      newErrors.username = "This field is required";
+    } else if (!usernameRegex.test(formData.username)) {
+      newErrors.username = "Username must only contain letters";
+    }
+
+    if (!formData.email) {
+      newErrors.email = "This field is required";
+    }
+
+    if (!formData.password) {
+      newErrors.password = "This field is required";
+    } else if (!passwordRegex.test(formData.password)) {
+      newErrors.password = "Must include caps,small-letter and symbol";
+    }
+
+    if (formData.password !== formData.confirm) {
+      newErrors.confirm = "Passwords do not match";
+    }
 
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
